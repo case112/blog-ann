ce58d8ae06d1cec8ada01657974fb94335140d13
Oskar <oskar.case@gmail.com>
2019-12-01 12:37:00 +0800

    Changed admin view

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from . models import Author, Category, Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+    #list_filter = ("status",)
+    search_fields = ['title', 'content']
+    #prepopulated_fields = {'slug': ('title',)}
+  
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
