f12464ba3efe82d1c256af9fdc7807d672de9c57
Oskar <oskar.case@gmail.com>
2019-12-02 11:55:00 +0800

    Added Publish and Draft options on Post app

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from . models import Author, Category, Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'timestamp')
-    #list_filter = ("status",)
+    list_display = ('title', 'slug', 'status','timestamp')
+    list_filter = ("status",)
     search_fields = ['title', 'content']
-    #prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}
   
 
 admin.site.register(Author)
