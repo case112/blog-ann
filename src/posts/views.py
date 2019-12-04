bc1d6d1f3590cc44db2b51367a8e2749f36db8bb
Oskar <oskar.case@gmail.com>
2019-12-04 11:30:33 +0800

    Added SlugField to posts and url's. Changed 'next-page', 'previous-page' logic

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from next_prev import next_in_order, prev_in_order
 from . models import Post
+
 
 def index(request):
 	featured = Post.objects.filter(status=1, featured=True)
@@ -14,7 +16,7 @@ def index(request):
 def blog(request):
 	post_list = Post.objects.filter(status=1)
 	most_recent = Post.objects.filter(status=1).order_by('-created_on')[0:3]
-	paginator = Paginator(post_list, 6)
+	paginator = Paginator(post_list, 1)
 	page_request_var = 'page'
 	page = request.GET.get(page_request_var)
 	try:
@@ -31,9 +33,23 @@ def blog(request):
 	}
 	return render(request, 'blog.html', context)
 
-def post(request, id):
-	post = get_object_or_404(Post, id=id)
-	context = {
-		'post': post,
-	}
-	return render(request, 'post.html', context)
+def post(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+
+	try:
+		next_post = post.get_next_by_created_on()
+	except Post.DoesNotExist:
+		next_post = None
+
+	try:
+		previous_post = post.get_previous_by_created_on()
+	except Post.DoesNotExist:
+		previous_post = None
+
+	return render(request, 'post.html', {
+		'post': post, 
+		'next_post': next_post,
+		'previous_post': previous_post,
+		})
+
+
